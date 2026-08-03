54718fadd159ee685a486298b5b38068a4d44a31
HenriMarkus08 <humorhenri@gmail.com>
2026-08-03 20:08:10 -0300

    Update SequelasBackendApplication.java

--- sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/SequelasBackendApplication.java
+++ sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/SequelasBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SequelasBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SequelasBackendApplication.class, args);
     }
-
 }
