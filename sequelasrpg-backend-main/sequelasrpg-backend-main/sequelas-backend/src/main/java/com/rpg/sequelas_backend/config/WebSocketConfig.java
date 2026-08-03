456788fc004632f53de350c05e5ceaac5fe113cc
HenriMarkus08 <humorhenri@gmail.com>
2026-08-02 23:07:14 -0300

    Update WebSocketConfig.java

--- sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/config/WebSocketConfig.java
+++ sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.rpg.sequelasbackend.config;
+package com.rpg.sequelas_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
