e7926ffb538b21fa6b0864d2caf9dc6d79749686
HenriMarkus08 <humorhenri@gmail.com>
2026-08-03 20:01:07 -0300

    Update GameActionMessage.java

--- sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/dto/GameActionMessage.java
+++ sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/dto/GameActionMessage.java
@@ -1,12 +1,12 @@
-package com.rpg.sequelasbackend.dto;
+package com.rpg.sequelas_backend.dto;
 
 import lombok.Data;
 import java.util.List;
 
 @Data
 public class GameActionMessage {
-    private String type;        // Ex: "REGISTER", "UPDATE_PARTICIPANT", "PARTICIPANT_LIST", "HP_UPDATE", "CHAT_LOG"
-    private String sender;      // Nome do jogador
-    private String payloadJson; // Dados da ação em JSON
-    private List<ParticipantDto> participants; // Usado para enviar a lista completa
+    private String type;
+    private String sender;
+    private String payloadJson;
+    private List<ParticipantDto> participants;
 }
