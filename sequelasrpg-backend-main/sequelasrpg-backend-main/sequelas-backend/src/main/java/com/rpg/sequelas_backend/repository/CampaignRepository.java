0c5eb248b64e1fc588b128dfa845e6ffd33a6b9a
HenriMarkus08 <humorhenri@gmail.com>
2026-08-02 23:19:07 -0300

    Update CampaignRepository.java

--- sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/repository/CampaignRepository.java
+++ sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/repository/CampaignRepository.java
@@ -2,10 +2,11 @@ package com.rpg.sequelas_backend.repository;
 
 import com.rpg.sequelas_backend.model.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-public interface CampaignRepository extends JpaRepository {
-    
-    // O Spring Data JPA cria a consulta SQL automaticamente baseada no nome do método!
-    Optional findByRoomCode(String roomCode);
+@Repository
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+    Optional<Campaign> findByRoomCode(String roomCode);
 }
