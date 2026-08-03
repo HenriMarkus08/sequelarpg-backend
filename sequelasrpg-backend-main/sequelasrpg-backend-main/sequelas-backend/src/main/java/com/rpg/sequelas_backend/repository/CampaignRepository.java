251ecb066daf58fae7e23b5163952a066116a100
HenriMarkus08 <humorhenri@gmail.com>
2026-08-03 20:04:53 -0300

    Update CampaignRepository.java

--- sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/repository/CampaignRepository.java
+++ sequelasrpg-backend-main/sequelasrpg-backend-main/sequelas-backend/src/main/java/com/rpg/sequelas_backend/repository/CampaignRepository.java
@@ -2,11 +2,8 @@ package com.rpg.sequelas_backend.repository;
 
 import com.rpg.sequelas_backend.model.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
     Optional<Campaign> findByRoomCode(String roomCode);
 }
